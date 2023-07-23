1f2738e7749ff620db905b5f6c781eaba287bbb1
Piyush Prateek <piyushprateek231996@gmail.com>
2023-07-24 00:48:03 +0530

    created frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+import Header from "./component/layout/Header.tsx"
+
+
+function App() {
+
+  return (
+    <Header />
+  )
+}
+
+export default App
---
+++ frontend/src/component/layout/Header.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+import { ReactNavbar } from "overlay-navbar"
+
+const Header = () => {
+    return (
+        <ReactNavbar />
+    )
+}
+
+export default Header
