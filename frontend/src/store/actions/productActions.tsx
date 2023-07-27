98108e2790c5e21256a8d745662fe50ad8f30e4a
Piyush Prateek <piyushprateek231996@gmail.com>
2023-07-27 16:23:41 +0530

    productActions.tsx -> fixed catch(error) ->store.ts- added exports=RootState,AppDispatch,getStateType

--- frontend/src/store/actions/productActions.tsx
+++ frontend/src/store/actions/productActions.tsx
@@ -1,9 +1,8 @@
-import axios from "axios"
+import axios, { AxiosError } from "axios"
 import { Dispatch } from 'redux';
 
 import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAIL, CLEAR_ERRORS } from "../slice/productSlice"
 import { ActionCreatorWithoutPayload } from "@reduxjs/toolkit";
-
 
 
 // Defining the parameters that the action accepts
@@ -15,32 +14,32 @@ interface GetProductParams {
     ratings?: number;
 }
 
-export const getProduct =
+interface ErrorResponse {
+    message: string;
+}
 
-    async (params: GetProductParams, dispatch: Dispatch) => {
-        try {
-            const { keyword = "", currentPage = 1, price = [0, 25000], category, ratings = 0 } = params
-            dispatch({ type: ALL_PRODUCT_REQUEST });
+//get All Products
+export const getProduct = (params: GetProductParams) => async (dispatch: Dispatch) => {
+    try {
+        dispatch(ALL_PRODUCT_REQUEST())
+        const { keyword = "", currentPage = 1, price = [0, 25000], category, ratings = 0 } = params
 
-            let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
+        let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
 
-            if (category) {
-                link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
-            }
-
-            const { data } = await axios.get(link);
-
-            dispatch({
-                type: ALL_PRODUCT_SUCCESS,
-                payload: data,
-            });
-        } catch (error) {
-            dispatch({
-                type: ALL_PRODUCT_FAIL,
-                payload: ((error).response?.data.message as string) || "Error Occurred",
-            });
+        if (category) {
+            link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
         }
-    };
+
+        const { data } = await axios.get(link);
+
+        dispatch(ALL_PRODUCT_SUCCESS(data));
+    }
+    catch (error) {
+        const axiosError = error as AxiosError<ErrorResponse>;
+        const message = axiosError?.response?.data?.message || "Error Occurred";
+        dispatch(ALL_PRODUCT_FAIL(message));
+    }
+};
 
 
 // Clearing Errors
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -9,4 +9,9 @@ export const store = configureStore({
     // Other store configurations if needed...
 });
 
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+export type getStateType = () => RootState
 export default store;
