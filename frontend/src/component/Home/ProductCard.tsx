45a3cdb6a3f94e0f6958020167933b147d04c966
Piyush Prateek <piyushprateek231996@gmail.com>
2023-07-28 01:57:41 +0530

    ProductCard.tsx -> added import { Rating } from '@mui/material'

--- frontend/src/component/Home/ProductCard.tsx
+++ frontend/src/component/Home/ProductCard.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import ReactStars from "react-rating-stars-component"
 import "./Home.css"
-
+import { Rating } from '@mui/material'
 
 interface Product {
     _id: string;
@@ -17,19 +16,16 @@ interface ProductCardProps {
 }
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
     const options = {
-        edit: false,
-        color: "rgba(20,20,20,0.1)",
-        activeColor: "tomato",
-        size: window.innerWidth < 600 ? 20 : 25,
         value: product.ratings,
-        isHalf: true
+        readOnly: true,
+        precision: 0.5,
     }
     return (
         <Link className='productCard' to={`/product/${product._id}`}>
             <img src={product.images[0].url} alt={product.name} />
             <p>{product.name}</p>
             <div>
-                <ReactStars {...options} />{" "}
+                <Rating {...options} />{" "}
                 <span className="productCardSpan">
                     {" "}
                     ({product.numOfReviews} Reviews)
