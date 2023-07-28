e9d8eee6fa827972d8464d0830f02df71de445ff
Piyush Prateek <piyushprateek231996@gmail.com>
2023-07-29 03:45:12 +0530

    created store for order

---
+++ frontend/src/store/actions/orderActions.tsx
@@ -0,0 +1,121 @@
+import axios, { AxiosError } from "axios";
+import { createOrderRequest, createOrderSuccess, createOrderFail, myOrdersRequest, myOrdersSuccess, myOrdersFail, allOrdersRequest, allOrdersSuccess, allOrdersFail, updateOrderRequest, updateOrderSuccess, updateOrderFail, deleteOrderRequest, deleteOrderSuccess, deleteOrderFail, orderDetailsRequest, orderDetailsSuccess, orderDetailsFail, clearErrors } from "../slice/orderSlice";
+import { Dispatch } from '@reduxjs/toolkit';
+
+export interface Order {
+    // Define the properties of an order object
+    // For example: _id, product, quantity, price, etc.
+    _id: string;
+    product: string;
+    quantity: number;
+    price: number;
+    // ...
+}
+
+interface ErrorResponse {
+    message: string;
+}
+
+// Create Order
+export const createOrder = (order: Order) => async (dispatch: Dispatch) => {
+    try {
+        dispatch(createOrderRequest());
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+        const { data } = await axios.post("/api/v1/order/new", order, config);
+
+        dispatch(createOrderSuccess(data));
+    } catch (error) {
+        const axiosError = error as AxiosError<ErrorResponse>;
+        const message = axiosError?.response?.data?.message || "Error Occurred";
+        dispatch(createOrderFail(message))
+    }
+};
+
+// My Orders
+export const myOrders = () => async (dispatch: Dispatch) => {
+    try {
+        dispatch(myOrdersRequest());
+
+        const { data } = await axios.get("/api/v1/orders/me");
+
+        dispatch(myOrdersSuccess(data.orders));
+    } catch (error) {
+        const axiosError = error as AxiosError<ErrorResponse>;
+        const message = axiosError?.response?.data?.message || "Error Occurred";
+        dispatch(myOrdersFail(message));
+    }
+};
+
+// Get All Orders (admin)
+export const getAllOrders = () => async (dispatch: Dispatch) => {
+    try {
+        dispatch(allOrdersRequest());
+
+        const { data } = await axios.get("/api/v1/admin/orders");
+
+        dispatch(allOrdersSuccess(data.orders));
+    } catch (error) {
+        const axiosError = error as AxiosError<ErrorResponse>;
+        const message = axiosError?.response?.data?.message || "Error Occurred";
+        dispatch(allOrdersFail(message));
+    }
+};
+
+// Update Order
+export const updateOrder = (id: string, order: Order) => async (dispatch: Dispatch) => {
+    try {
+        dispatch(updateOrderRequest());
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+        const { data } = await axios.put(`/api/v1/admin/order/${id}`, order, config);
+
+        dispatch(updateOrderSuccess(data.success));
+    } catch (error) {
+        const axiosError = error as AxiosError<ErrorResponse>;
+        const message = axiosError?.response?.data?.message || "Error Occurred";
+        dispatch(updateOrderFail(message));
+    }
+};
+
+// Delete Order
+export const deleteOrder = (id: string) => async (dispatch: Dispatch) => {
+    try {
+        dispatch(deleteOrderRequest());
+
+        const { data } = await axios.delete(`/api/v1/admin/order/${id}`);
+
+        dispatch(deleteOrderSuccess(data.success));
+    } catch (error) {
+        const axiosError = error as AxiosError<ErrorResponse>;
+        const message = axiosError?.response?.data?.message || "Error Occurred";
+        dispatch(deleteOrderFail(message));
+    }
+};
+
+// Get Order Details
+export const getOrderDetails = (id: string) => async (dispatch: Dispatch) => {
+    try {
+        dispatch(orderDetailsRequest());
+
+        const { data } = await axios.get(`/api/v1/order/${id}`);
+
+        dispatch(orderDetailsSuccess(data.order));
+    } catch (error) {
+        const axiosError = error as AxiosError<ErrorResponse>;
+        const message = axiosError?.response?.data?.message || "Error Occurred";
+        dispatch(orderDetailsFail(message));
+    }
+};
+// Clearing Errors
+export const clearAllErrors = () => async (dispatch: Dispatch) => {
+    dispatch(clearErrors());
+};
---
+++ frontend/src/store/slice/orderSlice.tsx
@@ -0,0 +1,136 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface OrderState {
+    loading: boolean;
+    orders: Order[];
+    order: Order | null;
+    isUpdated: boolean;
+    isDeleted: boolean;
+    error: string | null;
+}
+export interface Order {
+    _id: string;
+    // Add other properties of an order as needed
+}
+const initialState: OrderState = {
+    loading: false,
+    orders: [],
+    order: null,
+    isUpdated: false,
+    isDeleted: false,
+    error: null,
+};
+
+const orderSlice = createSlice({
+    name: "order",
+    initialState,
+    reducers: {
+        createOrderRequest(state) {
+            state.loading = true;
+        },
+        createOrderSuccess(state, action: PayloadAction<Order>) {
+            state.loading = false;
+            state.order = action.payload;
+        },
+        createOrderFail(state, action: PayloadAction<string>) {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        clearErrors(state) {
+            state.error = null;
+        },
+        myOrdersRequest(state) {
+            state.loading = true;
+        },
+        myOrdersSuccess(state, action: PayloadAction<Order[]>) {
+            state.loading = false;
+            state.orders = action.payload;
+        },
+        myOrdersFail(state, action: PayloadAction<string>) {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        allOrdersRequest(state) {
+            state.loading = true;
+        },
+        allOrdersSuccess(state, action: PayloadAction<Order[]>) {
+            state.loading = false;
+            state.orders = action.payload;
+        },
+        allOrdersFail(state, action: PayloadAction<string>) {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        updateOrderRequest(state) {
+            state.loading = true;
+        },
+        deleteOrderRequest(state) {
+            state.loading = true;
+        },
+        updateOrderSuccess(state, action: PayloadAction<boolean>) {
+            state.loading = false;
+            state.isUpdated = action.payload;
+        },
+        deleteOrderSuccess(state, action: PayloadAction<boolean>) {
+            state.loading = false;
+            state.isDeleted = action.payload;
+        },
+        updateOrderFail(state, action: PayloadAction<string>) {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        deleteOrderFail(state, action: PayloadAction<string>) {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        updateOrderReset(state) {
+            state.isUpdated = false;
+        },
+        deleteOrderReset(state) {
+            state.isDeleted = false;
+        },
+        orderDetailsRequest(state) {
+            state.loading = true;
+        },
+        orderDetailsSuccess(state, action: PayloadAction<Order>) {
+            state.loading = false;
+            state.order = action.payload;
+        },
+        orderDetailsFail(state, action: PayloadAction<string>) {
+            state.loading = false;
+            state.error = action.payload;
+        },
+    },
+});
+
+export const {
+    createOrderRequest,
+    createOrderSuccess,
+    createOrderFail,
+
+    clearErrors,
+    myOrdersRequest,
+    myOrdersSuccess,
+    myOrdersFail,
+
+    allOrdersRequest,
+    allOrdersSuccess,
+    allOrdersFail,
+
+    updateOrderRequest,
+    updateOrderSuccess,
+
+    deleteOrderRequest,
+    deleteOrderSuccess,
+    deleteOrderFail,
+    deleteOrderReset,
+
+    updateOrderFail,
+    updateOrderReset,
+
+    orderDetailsRequest,
+    orderDetailsSuccess,
+    orderDetailsFail,
+} = orderSlice.actions;
+
+export default orderSlice.reducer;
